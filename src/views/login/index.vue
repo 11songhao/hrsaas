11b2d6cf13d7c0c89361bdfee4834fa3b07967fc
songhao <1146987523@qq.com>
2022-07-31 10:08:09 +0800

    second commit

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,14 +1,11 @@
 <template>
   <div class="login-container">
     <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
-
-      <div class="title-container">
-        <h3 class="title">Login Form</h3>
-      </div>
-
-      <el-form-item prop="username">
+     <div class="login-form">
+        <img class="login-logo" src="http://likede2-admin.itheima.net/img/logo.595745bd.png" alt="">
+       <el-form-item prop="username">
         <span class="svg-container">
-          <svg-icon icon-class="user" />
+         <i class="el-icon-mobile"></i>
         </span>
         <el-input
           ref="username"
@@ -23,7 +20,7 @@
 
       <el-form-item prop="password">
         <span class="svg-container">
-          <svg-icon icon-class="password" />
+          <i class="el-icon-lock"></i>
         </span>
         <el-input
           :key="passwordType"
@@ -40,14 +37,17 @@
           <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
         </span>
       </el-form-item>
-
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">Login</el-button>
-
-      <div class="tips">
-        <span style="margin-right:20px;">username: admin</span>
-        <span> password: any</span>
-      </div>
-
+      <el-form-item>
+        <span class="svg-container">
+          <i class="el-icon-circle-check"></i>
+        </span>
+  <el-input v-model="input" placeholder="请输入验证码" />
+<span class="security">
+    <img class="securityImg" src="https://likede2-java.itheima.net/api/user-service/user/imageCode/SWbZhns43uPUpoi3oubbost359FHlysc" alt="">
+  </span>
+      </el-form-item>
+      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">登录</el-button>
+     </div>
     </el-form>
   </div>
 </template>
@@ -132,42 +132,61 @@ export default {
 $bg:#283443;
 $light_gray:#fff;
 $cursor: #fff;
+$dark_gray:#889aa4;
 
 @supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
   .login-container .el-input input {
     color: $cursor;
   }
 }
+.login-container{
+  width: 100%;
+  height: 100%;
+  line-height: 100%;
+  background: url(~@/assets/background.png) no-repeat;
+  position: relative;
+}
 
 /* reset element-ui css */
 .login-container {
-  .el-input {
+.el-input {
     display: inline-block;
     height: 47px;
     width: 85%;
-
     input {
       background: transparent;
-      border: 0px;
-      -webkit-appearance: none;
-      border-radius: 0px;
-      padding: 12px 5px 12px 15px;
-      color: $light_gray;
-      height: 47px;
-      caret-color: $cursor;
+     height: 47px;
+    border: 0px;
+    border-radius: 0px;
+    padding: 12px 5px 12px 15px;
+    color:$bg;
+    caret-color: $cursor;
+    -webkit-appearance: none;
 
       &:-webkit-autofill {
         box-shadow: 0 0 0px 1000px $bg inset !important;
         -webkit-text-fill-color: $cursor !important;
       }
-    }
-  }
 
+  }
+}
   .el-form-item {
-    border: 1px solid rgba(255, 255, 255, 0.1);
-    background: rgba(0, 0, 0, 0.1);
+    border: 1px solid $dark_gray;
+    background: #fff;
     border-radius: 5px;
     color: #454545;
+  }
+  .security{
+    position: relative;
+    width: 135px;
+    height: 50px;
+    .securityImg{
+      position: absolute;
+    height: 53px;
+    width: 126px;
+    top: -17px;
+    left: -89px;
+  }
   }
 }
 </style>
@@ -177,19 +196,26 @@ $bg:#2d3a4b;
 $dark_gray:#889aa4;
 $light_gray:#eee;
 
-.login-container {
-  min-height: 100%;
-  width: 100%;
-  background-color: $bg;
-  overflow: hidden;
-
-  .login-form {
-    position: relative;
-    width: 520px;
-    max-width: 100%;
-    padding: 160px 35px 0;
-    margin: 0 auto;
-    overflow: hidden;
+.login-container .login-form {
+    position: absolute;
+    width: 518px;
+    height: 388px;
+    top: 50%;
+    left: 50%;
+    margin-top: -194px;
+    margin-left: -259px;
+    padding: 76px 35px 0;
+    background: #fff;
+    -webkit-box-shadow: 0 3px 70px 0 rgb(30 111 72 / 35%);
+    box-shadow: 0 3px 70px 0 rgb(30 111 72 / 35%);
+    border-radius: 10px;
+     .login-logo{
+    position: absolute;
+    width: 96px;
+    height: 96px;
+    top: -46px;
+    left: 50%;
+    margin-left: -48px;
   }
 
   .tips {
@@ -211,19 +237,6 @@ $light_gray:#eee;
     width: 30px;
     display: inline-block;
   }
-
-  .title-container {
-    position: relative;
-
-    .title {
-      font-size: 26px;
-      color: $light_gray;
-      margin: 0px auto 40px auto;
-      text-align: center;
-      font-weight: bold;
-    }
-  }
-
   .show-pwd {
     position: absolute;
     right: 10px;

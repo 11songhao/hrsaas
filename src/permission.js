7fc9ab466777f98e9f72983c40d786dd810112f1
songhao <1146987523@qq.com>
2022-08-02 12:17:06 +0800

    wanchengle

---
+++ src/permission.js
@@ -0,0 +1,21 @@
+import router from '@/router'
+import store from '@/store'
+
+const whiteList = ['/login', '/404']
+router.beforeEach((to, from, next) => {
+  const token = store.state.user.token
+  if (token) {
+    if (to.path === '/login') {
+      next('/')
+    } else {
+      next()
+    }
+  } else {
+    const includes = whiteList.includes(to.path)
+    if (includes) {
+      next()
+    } else {
+      next('/login')
+    }
+  }
+})

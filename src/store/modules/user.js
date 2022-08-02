7e849ea0c30e86629cb26111a4d2e615a38a0717
songhao <1146987523@qq.com>
2022-08-02 10:45:56 +0800

    完成登录流程

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,6 +1,19 @@
+import { login } from "@/api/user";
 export default {
   namespaced: true,
-  state: {},
-  mutations: {},
-  actions: {}
+  state: { 
+    token: ''
+   },
+  mutations: {
+    setToken(state, payLoad) {
+      state.token = payLoad
+    }
+  },
+  actions: {
+    async getToken(context, payLoad) {
+      const res = await login(payLoad)
+      console.log(res);
+      context.commit('setToken', res)
+    }
+  }
 }
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,125 +1,90 @@
 <template>
   <div class="login-container">
-    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
-     <div class="login-form">
-        <img class="login-logo" src="http://likede2-admin.itheima.net/img/logo.595745bd.png" alt="">
-       <el-form-item prop="username">
-        <span class="svg-container">
-         <i class="el-icon-mobile"></i>
-        </span>
-        <el-input
-          ref="username"
-          v-model="loginForm.username"
-          placeholder="Username"
-          name="username"
-          type="text"
-          tabindex="1"
-          auto-complete="on"
-        />
+    <el-form
+      ref="loginForm"
+      class="login-form"
+      auto-complete="on"
+      label-position="left"
+      :model="loginForm"
+      :rules="loginFormRules"
+    >
+      <!-- 放置标题图片 @是设置的别名-->
+      <div class="title-container">
+        <h3 class="title">
+          <img src="@/assets/common/login-logo.png" alt="" />
+        </h3>
+      </div>
+      <el-form-item prop="mobile">
+        <!-- 使用element ui内置图标 -->
+        <span class="svg-container el-icon-user-solid"></span>
+        <el-input v-model="loginForm.mobile"></el-input>
       </el-form-item>
-
       <el-form-item prop="password">
+        <!-- 使用自己svg图标 -->
         <span class="svg-container">
-          <i class="el-icon-lock"></i>
+          <svg-icon iconClass="password"></svg-icon>
         </span>
-        <el-input
-          :key="passwordType"
-          ref="password"
-          v-model="loginForm.password"
-          :type="passwordType"
-          placeholder="Password"
-          name="password"
-          tabindex="2"
-          auto-complete="on"
-          @keyup.enter.native="handleLogin"
-        />
-        <span class="show-pwd" @click="showPwd">
-          <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
-        </span>
+        <el-input type="password" v-model="loginForm.password"></el-input>
       </el-form-item>
-      <el-form-item>
-        <span class="svg-container">
-          <i class="el-icon-circle-check"></i>
-        </span>
-  <el-input v-model="input" placeholder="请输入验证码" />
-<span class="security">
-    <img class="securityImg" src="https://likede2-java.itheima.net/api/user-service/user/imageCode/SWbZhns43uPUpoi3oubbost359FHlysc" alt="">
-  </span>
-      </el-form-item>
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.native.prevent="handleLogin">登录</el-button>
-     </div>
+      <el-button
+        class="loginBtn"
+        type="primary"
+        style="width: 100%; margin-bottom: 30px"
+        @click="login"
+        :isLoad="true"
+        >登录</el-button
+      >
+
+      <div class="tips">
+        <span style="margin-right: 20px">账号: 13800000002</span>
+        <span> 密码: 123456</span>
+      </div>
     </el-form>
   </div>
 </template>
 
 <script>
-import { validUsername } from '@/utils/validate'
-
 export default {
   name: 'Login',
   data() {
-    const validateUsername = (rule, value, callback) => {
-      if (!validUsername(value)) {
-        callback(new Error('Please enter the correct user name'))
-      } else {
-        callback()
-      }
-    }
-    const validatePassword = (rule, value, callback) => {
-      if (value.length < 6) {
-        callback(new Error('The password can not be less than 6 digits'))
-      } else {
-        callback()
-      }
-    }
     return {
       loginForm: {
-        username: 'admin',
-        password: '111111'
+        mobile: '13800000002',
+        password: '123456'
       },
-      loginRules: {
-        username: [{ required: true, trigger: 'blur', validator: validateUsername }],
-        password: [{ required: true, trigger: 'blur', validator: validatePassword }]
-      },
-      loading: false,
-      passwordType: 'password',
-      redirect: undefined
-    }
-  },
-  watch: {
-    $route: {
-      handler: function(route) {
-        this.redirect = route.query && route.query.redirect
-      },
-      immediate: true
-    }
-  },
-  methods: {
-    showPwd() {
-      if (this.passwordType === 'password') {
-        this.passwordType = ''
-      } else {
-        this.passwordType = 'password'
+      loginFormRules: {
+        mobile: [
+          { required: true, message: '请填写手机号', trigger: 'blur' },
+          {
+            pattern: /^(?:(?:\+|00)86)?1[3-9]\d{9}$/,
+            message: '手机号码格式不正确'
+          }
+        ],
+        password: [
+          { required: true, message: '请填写密码', trigger: 'blur' }
+          // {
+          //   pattern:
+          //     /^(?![a-zA-Z]+$)(?![A-Z0-9]+$)(?![A-Z\W_!@#$%^&*`~()-+=]+$)(?![a-z0-9]+$)(?![a-z\W_!@#$%^&*`~()-+=]+$)(?![0-9\W_!@#$%^&*`~()-+=]+$)[a-zA-Z0-9\W_!@#$%^&*`~()-+=]/,
+          //   message: '密码格式为大写字母/小写字母/数字/特殊符号，任意三项组合'
+          // }
+        ]
       }
-      this.$nextTick(() => {
-        this.$refs.password.focus()
-      })
-    },
-    handleLogin() {
-      this.$refs.loginForm.validate(valid => {
-        if (valid) {
-          this.loading = true
-          this.$store.dispatch('user/login', this.loginForm).then(() => {
-            this.$router.push({ path: this.redirect || '/' })
-            this.loading = false
-          }).catch(() => {
-            this.loading = false
-          })
-        } else {
-          console.log('error submit!!')
-          return false
-        }
-      })
+    }
+  },
+  watch: {},
+  methods: {
+    async login() {
+      this.isLoad=true
+      try {
+        await this.$refs.loginForm.validate()
+        console.log('表单验证通过，可以发送请求')
+        await this.$store.dispatch('user/getToken', this.loginForm)
+        this.$router.push('/')
+        console.log('23123');
+        this.$message.success('登陆成功')
+      } finally{
+        this.isLoad=false
+      }
     }
   }
 }
@@ -129,93 +94,74 @@ export default {
 /* 修复input 背景不协调 和光标变色 */
 /* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
 
-$bg:#283443;
-$light_gray:#fff;
+$bg: #283443;
+$light_gray: #68b0fe;
 $cursor: #fff;
-$dark_gray:#889aa4;
 
 @supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
   .login-container .el-input input {
     color: $cursor;
   }
 }
-.login-container{
-  width: 100%;
-  height: 100%;
-  line-height: 100%;
-  background: url(~@/assets/background.png) no-repeat;
-  position: relative;
-}
 
 /* reset element-ui css */
 .login-container {
-.el-input {
+  .loginBtn {
+    background: #407ffe;
+    height: 64px;
+    line-height: 32px;
+    font-size: 24px;
+  }
+  .el-input {
     display: inline-block;
     height: 47px;
     width: 85%;
+
     input {
       background: transparent;
-     height: 47px;
-    border: 0px;
-    border-radius: 0px;
-    padding: 12px 5px 12px 15px;
-    color:$bg;
-    caret-color: $cursor;
-    -webkit-appearance: none;
+      border: 0px;
+      -webkit-appearance: none;
+      border-radius: 0px;
+      padding: 12px 5px 12px 15px;
+      color: $light_gray;
+      height: 47px;
+      caret-color: $cursor;
 
       &:-webkit-autofill {
         box-shadow: 0 0 0px 1000px $bg inset !important;
         -webkit-text-fill-color: $cursor !important;
       }
-
+    }
   }
-}
+
   .el-form-item {
-    border: 1px solid $dark_gray;
-    background: #fff;
+    border: 1px solid rgba(255, 255, 255, 0.1);
+    background: rgba(255, 255, 255, 0.7); // 输入登录表单的背景色
     border-radius: 5px;
     color: #454545;
-  }
-  .security{
-    position: relative;
-    width: 135px;
-    height: 50px;
-    .securityImg{
-      position: absolute;
-    height: 53px;
-    width: 126px;
-    top: -17px;
-    left: -89px;
-  }
   }
 }
 </style>
 
 <style lang="scss" scoped>
-$bg:#2d3a4b;
-$dark_gray:#889aa4;
-$light_gray:#eee;
+$bg: #2d3a4b;
+$dark_gray: #889aa4;
+$light_gray: #eee;
 
-.login-container .login-form {
-    position: absolute;
-    width: 518px;
-    height: 388px;
-    top: 50%;
-    left: 50%;
-    margin-top: -194px;
-    margin-left: -259px;
-    padding: 76px 35px 0;
-    background: #fff;
-    -webkit-box-shadow: 0 3px 70px 0 rgb(30 111 72 / 35%);
-    box-shadow: 0 3px 70px 0 rgb(30 111 72 / 35%);
-    border-radius: 10px;
-     .login-logo{
-    position: absolute;
-    width: 96px;
-    height: 96px;
-    top: -46px;
-    left: 50%;
-    margin-left: -48px;
+.login-container {
+  min-height: 100%;
+  width: 100%;
+  overflow: hidden;
+  background-image: url('~@/assets/common/login.jpg'); // 设置背景图片
+  background-position: center; // 将图片位置设置为充满整个屏幕
+
+  .login-form {
+    position: relative;
+    width: 520px;
+    max-width: 100%;
+    padding: 160px 35px 0;
+    margin: 0 auto;
+    overflow: hidden;
   }
 
   .tips {
@@ -236,6 +182,24 @@ $light_gray:#eee;
     vertical-align: middle;
     width: 30px;
     display: inline-block;
+  }
+
+  .title-container {
+    position: relative;
+
+    .title {
+      font-size: 26px;
+      color: $light_gray;
+      margin: 0px auto 40px auto;
+      text-align: center;
+      font-weight: bold;
+    }
+  }
+  .loginBtn {
+    background: #407ffe;
+    height: 64px;
+    line-height: 32px;
+    font-size: 24px;
   }
   .show-pwd {
     position: absolute;

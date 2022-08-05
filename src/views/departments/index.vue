beec1311c5184b9fbfdca2709c700088979c379f
songhao <1146987523@qq.com>
2022-08-05 11:33:31 +0800

    ssa

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,19 +1,44 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>组织架构</h2>
+      <el-card class="box-card">
+        <TreeTools :treeNode="company" :isRoot="true"/>
+        <el-tree :data="treeData" :props="defaultProps" default-expand-all>
+          <template v-slot="{ data }">
+            <TreeTools :treeNode="data" />
+          </template>
+        </el-tree>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
+import TreeTools from './commponents/tree-tools'
 export default {
   data() {
-    return {}
+    return {
+      treeData: [
+        {
+          name: '总裁办',
+          children: [{ name: '董事会' }]
+        },
+        { name: '行政部' },
+        { name: '人事部' }
+      ],
+      defaultProps: {
+        label: 'name'
+        // children: 'child'
+      },
+      company: {
+        name: '传智教育',
+        manager: '负责人'
+      }
+    }
   },
-
-  created() {},
-
+  components: {
+    TreeTools
+  },
   methods: {}
 }
 </script>
